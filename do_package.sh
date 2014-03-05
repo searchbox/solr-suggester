082bd7cd322cccf47edf8116f3e1939a229cb296
Jonathan Rey <jonatrey@reziew.com>
2014-03-05 16:46:06 +0100

    Putting back the line to generate fat jars.

--- do_package.sh
+++ do_package.sh
@@ -1,6 +1,6 @@
 mvn -T compile package -DskipTests=true
 mkdir ../deploy-dir/searchbox-suggester
-cp ./target/searchbox-suggester-1.0-SNAPSHOT-jar-with-dependencies.jar  ../deploy-dir/searchbox-suggester/searchbox-suggester-1.38.jar
+cp ./target/searchbox-suggester-1.1-jar-with-dependencies.jar  ../deploy-dir/searchbox-suggester/searchbox-suggester-1.38.jar
 cp README*  ../deploy-dir/searchbox-suggester
 cd ../deploy-dir/searchbox-suggester
 zip searchbox-suggester.zip *
--- doall.sh
+++ doall.sh
@@ -1,4 +1,3 @@
 mvn -T compile package -DskipTests=true
-cp ./target/searchbox-suggester-1.0-SNAPSHOT-jar-with-dependencies.jar /salsasvn/projects/solr-4.1.0/example/solr/solrLib
-cp ./target/searchbox-suggester-1.0-SNAPSHOT-jar-with-dependencies.jar /salsasvn/searchbox-server/solr/solrLib
-
+cp ./target/searchbox-suggester-1.1-jar-with-dependencies.jar /projects/solr-4.1.0/example/solr/solrLib
+cp ./target/searchbox-suggester-1.1-jar-with-dependencies.jar /searchbox-server/solr/solrLib
