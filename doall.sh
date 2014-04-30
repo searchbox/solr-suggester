e45f69b846abda1af6d63ea2c81942843bfbe59b
Jonathan Rey <jonatrey@reziew.com>
2014-04-30 16:48:33 +0200

    Removing dummy files

--- doall.sh
+++
@@ -1,3 +0,0 @@
-mvn -T compile package -DskipTests=true
-cp ./target/searchbox-suggester-1.1-jar-with-dependencies.jar /projects/solr-4.1.0/example/solr/solrLib
-cp ./target/searchbox-suggester-1.1-jar-with-dependencies.jar /searchbox-server/solr/solrLib
